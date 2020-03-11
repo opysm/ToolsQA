3aef9e656f936659886baa18f01a831201aab92e
Oleh Pysmennyi <opysm@softserveinc.com>
2020-03-11 21:42:08 +0200

    added 3,4 tasks

--- ToolsQA.Test/SearchTests.cs
+++ ToolsQA.Test/SearchTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ToolsQA.Framework;
 using ToolsQA.UI;
@@ -16,10 +17,41 @@ namespace ToolsQA.Test
         public void SetUp()
         {
             DriverFactory.Initialize("http://automationpractice.com/index.php");
+            DriverFactory.Get().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
 
         [Test]
-        public void SearchIncorrectText()
+        public void SearchFirstProductTest()
+        {
+            string searchText = "Printed";
+            string expectedFirstProductName = "Printed Summer Dress";
+            string expectedFirstProductPrice = "$28.98";
+
+            ToolsQAPages.IndexPage.SetSearchText(searchText);
+            ToolsQAPages.IndexPage.ClickSearch();
+            
+            var firstProductName = ToolsQAPages.SearchPage.GetFirstProductName();
+            var firstProductPrice = ToolsQAPages.SearchPage.GetFirstProductPrice();
+            
+            Assert.AreEqual(expectedFirstProductName, firstProductName);
+            Assert.AreEqual(expectedFirstProductPrice, firstProductPrice);
+        }
+
+        [Test]
+        public void ProductQuantityTest()
+        {
+            string searchText = "Printed";
+            int expectedProductQuantity = 5;
+
+            ToolsQAPages.IndexPage.SetSearchText(searchText);
+            ToolsQAPages.IndexPage.ClickSearch();
+            var productQuantity = ToolsQAPages.SearchPage.GetProductQuantity();
+
+            Assert.AreEqual(expectedProductQuantity, productQuantity);
+        }
+
+        [Test]
+        public void SearchIncorrectTextTest()
         {
             string searchText = "aaaaaaaaaaaaa";
             string expectedSearchWarning = $"No results were found for your search \"{searchText}\"";
---
+++ ToolsQA.UI/Pages/SearchPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolsQA.Framework;
+
+namespace ToolsQA.UI.Page
+{
+    public class SearchPage
+    {
+		#region Page Mapping
+
+		private IWebElement SearchWarning => DriverFactory.Get().FindElement(By.ClassName("alert-warning"));
+		private IReadOnlyCollection<IWebElement> products => DriverFactory.Get().FindElements(By.XPath("//div[@class='product-container']"));
+		private IWebElement FirstProductName => DriverFactory.Get().FindElement(By.XPath("//div[@class='product-container']/div/h5/a"));
+		private IWebElement FirstProductPrice => DriverFactory.Get().FindElement(By.XPath("(//span[@class='price product-price'])[2]"));
+
+		#endregion Page Mapping
+
+		#region Page Objects
+
+		public string SearchText
+		{
+			get { return SearchWarning.Text; }
+		}
+
+		public int GetProductQuantity()
+		{
+			return products.Count;
+		}
+
+		public string GetFirstProductName()
+		{
+			return FirstProductName.Text;
+		}
+
+		public string GetFirstProductPrice()
+		{
+			return FirstProductPrice.Text;
+		}
+
+		#endregion Page Objects
+	}
+}
